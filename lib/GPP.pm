be1a3532d308e639a9c7044bb0da6801aaaa15f3
Charles Boyd <csboyd07@gmail.com>
2013-05-23 01:48:38 -0500

    dynamically load/unload GPP::Pari object with pause()/resume() functions

--- lib/GPP.pm
+++ lib/GPP.pm
@@ -34,31 +34,56 @@ sub evaluate {
 
     chomp( $expr );
 
+    my $result = { };
+
+    if ( $self->{'paused'} ) {
+	if ( $self->{'paused'} && $expr =~ qr/resume/ ) {
+	    $result->{input} = $expr;
+	    $result->{output} = $self->resume();
+	    $result->{type}   = '_META_';
+	    return $result;
+	}
+	elsif ( $expr !~ qr/resume/ ) {
+	    $result->{input} = $expr;
+	    $result->{output} = 'paused: please resume() to continue';
+	    $result->{type}   = '_META_';
+	    return $result;
+	}
+	else {
+	    die 'unknown state';
+	}
+    }
+
+
     my( $pari, $stack ) = ( $self->{'pari'}, $self->{stack} );
 
     my $histsize_cache = $self->history_size();
-
-    my $result = { };
 
     if ( $expr =~ /quit\(\)/ || $expr =~ /quit/ ) {
 	$self->quit();
     } elsif ( $expr =~ /(\?{1,2})([a-z]+)/g ) {
 	my $help = 'help(' . "$2" . ')';
 	$result->{input} = "$help";
-	$result->{output} = $pari->evaluate_cmd($help);
+	$result->{output} = $pari->evaluate_cmd($help) // 'paused';
 	$result->{type} = '_HELP_';
     } elsif ( $expr =~ /version\(\)/ ) {
 	$result->{input} = "$expr";
 	$result->{output} = $self->get_version();
 	$result->{type} = 't_STR';
+    } elsif ( $expr =~ /pause/ ) {
+	$result->{input} = 'pause()';
+	$result->{output} = $self->pause();
+	$result->{type}   = '_META_';
+    } elsif ( $expr =~ /resume/ ) {
+	$result->{input} = 'resume()';
     } elsif ( $expr =~ /^\\.*/ ) {
 	$result->{input} = "$expr";
 	$result->{output} = $self->escape("$expr");
 	$result->{type} = '_META_';
     } else {
 	$result->{input} = "$expr";
-	$result->{output} = $pari->evaluate_cmd($expr);
-	$result->{type} = $pari->result_type($expr);
+	$result->{output} = $pari->evaluate_cmd($expr) // 'paused';
+	$result->{type} = $pari->result_type($expr) // 'paused';
     }
 
     my $histsize = $self->history_size();
@@ -87,7 +112,7 @@ sub escape {
     } elsif ( $expr =~ /^(\\)(e)(.*)/ ) {
 	return $self->{'disable_eval'} || ( eval($3) );
     } elsif ( $expr =~ /^(\\)(.*)/ ) {
-	return $pari->escape_cmd("$1"."$2");
+	return $pari->escape_cmd("$1"."$2") // 'paused';
     } else {
 	return "";
     }
@@ -132,6 +157,37 @@ sub license {
 			 )
 		     );
     return $license;
+}
+
+sub pause {
+    my ( $self ) = @_;
+    if ( ! $self->{'paused'} ) {
+	$self->{'pari'}->quit();
+	$self->{'paused'} = 1;
+	return 'paused';
+    }
+    return 0;
+}
+
+sub resume {
+    my ( $self ) = @_;
+    if ( $self->{'paused'} ) {
+	$self->{'pari'} = undef;
+	$self->{'pari'} = GPP::Pari->new();
+	$self->{'pari'}->init();
+	$self->{'paused'} = 0;
+	return 'resumed';
+    }
+    return 0;
+}
+
+sub reload {
+    my ( $self ) = @_;
+    $self->{'pari'}->quit();
+    $self->{'pari'} = GPP::Pari->new();
+    $self->{'pari'}->init();
+    $self->{'stack'} = GPP::Stack->new();
+    $self->{'histsize_cache'} = 0;
 }
 
 sub quit {
